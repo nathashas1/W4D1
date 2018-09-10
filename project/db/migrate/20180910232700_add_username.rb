41f8b656844296a69ebb3ea86f222808b3a5dae5
‘nathashas1’ <nathasha36@gmail.com>
2018-09-10 16:49:41 -0700

    create rails artwork api

---
+++ project/db/migrate/20180910232700_add_username.rb
@@ -0,0 +1,8 @@
+class AddUsername < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :name
+    remove_column :users, :email
+    
+    add_column :users, :username, :string
+  end
+end
